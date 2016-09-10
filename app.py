395e674d68a601a256afb484bc9c1473b06997e2
saraxiang <yiqix@andrew.cmu.edu>
2016-09-10 14:00:42 -0400

    form and ui updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template
 from flask.ext.mongoengine import MongoEngine
+from forms import MainForm
 
 import sys
 sys.path.insert(0, 'rides-python-sdk/example')
@@ -11,9 +12,12 @@ import request_a_ride
 app = Flask(__name__)
 
 # use decorators to link the function to a url
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template('main.html')
+	form = MainForm(csrf_enabled = False)
+	if form.validate_on_submit():
+		return redirect('/welcome')
+	return render_template('main.html', form=form)
 
 @app.route('/welcome')
 def welcome():
@@ -27,7 +31,7 @@ def uber():
 def request():
 	ride_details = request_a_ride.get_ride_details()
   	return ride_details
-
+    
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
