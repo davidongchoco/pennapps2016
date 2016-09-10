e47af7c0b1d3b655f505de5e58c55e3ae1e50bfe
David Ongchoco <davidong@sas.upenn.edu>
2016-09-09 23:05:52 -0400

    Flask Server Basic Setup
    basic server for flask

---
+++ app.py
@@ -0,0 +1,20 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template
+from flask.ext.mongoengine import MongoEngine
+
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
