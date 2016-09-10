f9c8841fdfb2b21e1960a8fc098c8e1303c83a0c
Jessica Xiang <jessicaxiang@Jessicas-MacBook-Pro.local>
2016-09-10 11:21:04 -0400

    uber api

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from flask import Flask, render_template
 from flask.ext.mongoengine import MongoEngine
 
+import sys
+sys.path.insert(0, 'rides-python-sdk/example')
+
+import request_a_ride
 
 # create the application object
 app = Flask(__name__)
@@ -14,6 +18,15 @@ def home():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
+
+@app.route('/uber')
+def uber():
+    return render_template('uber.html')  # render a template
+
+@app.route('/uber/request')
+def request():
+	ride_details = request_a_ride.get_ride_details()
+  	return ride_details
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
